a6bd9468a1d04a21fc11808370d9310dad411601
Kyle Dilbeck <kdilbeck@csumb.edu>
2019-04-19 22:29:48 +0000

    C++

--- mT.cpp
+++ mT.cpp
@@ -13,10 +13,13 @@ using namespace std;
 
 int test(int n); 
 void test2(int n); 
+void test3(int n, int k);
 
 int main()
 {
-    int n1; 
+    int n1,n2,n3; 
+    char input; 
+    input = 'Y';
     cout << "How many times would you like to recurse: " ;
     cin >> n1; 
     
@@ -25,6 +28,25 @@ int main()
     cout << endl<<"test 2" <<endl << endl; 
     
     test2(n1);
+    
+    cout << "Would you like to recurse a number? (Y/N)";
+    cin >> input;
+    
+    while(input == 'Y')
+    {
+        cout << "Please enter number to recurse: ";
+        cin >> n2; 
+        cout << "How many times would you like to recurse ->" << n2 << "<- ? "; 
+        cin >> n3; 
+        
+        test3(n2, n3); 
+        
+        cout << "Would you like to recurse another number? (Y/N) " ;
+        cin >> input; 
+        
+    }
+    
+    
     
 }
 
@@ -77,5 +99,31 @@ void test2(int n)
         cout << endl<<"sp " << n << endl; 
         test2(n-1);
         cout << "final  ";
+    }
+}
+
+void test3(int n, int k)
+{
+    for(int i = 0 ; i < k ; i ++)//info debug
+    {
+        cout << "bug " ; 
+    }
+    cout  << "N=" << k<<endl ;
+    if(k==0)//base case
+    {
+        cout << " B A S E   C A S E   R E A C H E D    @N=" << k << endl; 
+        return ; 
+    }
+    else if(k == 1)
+    {
+        cout << "Special case " << n << endl; ; 
+        test3(n*4 , k-1); 
+    }
+    else if(k > 1)
+    {
+        cout << "recursing " << n  << " ";
+        test3(n*4 , k-1);
+        
+        
     }
 }
